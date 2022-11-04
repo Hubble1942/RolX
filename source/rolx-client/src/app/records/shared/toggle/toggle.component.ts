716029854177ea04e9cb893fc9384e4ec712dbf7
Markus Frei <frm@mfen-dom.local>
2022-11-04 12:17:30 +0100

    added start and stop button

--- source/rolx-client/src/app/records/shared/toggle/toggle.component.ts
+++ source/rolx-client/src/app/records/shared/toggle/toggle.component.ts
@@ -3,13 +3,36 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'rolx-toggle',
   templateUrl: './toggle.component.html',
-  styleUrls: ['./toggle.component.scss']
+  styleUrls: ['./toggle.component.scss'],
 })
 export class ToggleComponent implements OnInit {
+  private startTime?: Date;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  startToggle(): void {
+    this.storeStartTime(new Date());
   }
 
+  startToggleDisabled(): boolean {
+    return !!this.getStartTime();
+  }
+
+  storeStartTime(startTime: Date | undefined): void {
+    this.startTime = startTime;
+  }
+
+  getStartTime(): Date | undefined {
+    return this.startTime;
+  }
+
+  stopToggle(): void {
+    this.storeStartTime(undefined);
+  }
+
+  stopToggleDisabled(): boolean {
+    return !this.getStartTime();
+  }
 }
